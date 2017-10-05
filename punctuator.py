5a6d0bac4ffe145eb57b934ec9129c0c0bce32d6
aaron <aaron@scribie.com>
2017-10-05 15:03:09 -0700

    rewrote file to be used as a Punctuator class that accepts a model.pcl file which it loads once, changed functionality to use in-memory strings instead of txt files, added functions to properly clean text, removed functionality to restore with pauses because I haven't trained with them

--- punctuator.py
+++ punctuator.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from __future__ import division
 
+import re
+
 import models
 import data
 
@@ -11,166 +13,115 @@ import codecs
 import theano.tensor as T
 import numpy as np
 
-MAX_SUBSEQUENCE_LEN = 200
 
-def to_array(arr, dtype=np.int32):
-    # minibatch of 1 sequence as column
-    return np.array([arr], dtype=dtype).T
+class Punctuator():
 
-def convert_punctuation_to_readable(punct_token):
-    if punct_token == data.SPACE:
-        return " "
-    else:
-        return punct_token[0]
+    def __init__(self,model_file,max_subseq=200):
+        self.MAX_SUBSEQUENCE_LEN = max_subseq
 
-def restore_with_pauses(output_file, text, pauses, word_vocabulary, reverse_punctuation_vocabulary, predict_function):
-    i = 0
-    with codecs.open(output_file, 'w', 'utf-8') as f_out:
-        while True:
-
-            subsequence = text[i:i+MAX_SUBSEQUENCE_LEN]
-            subsequence_pauses = pauses[i:i+MAX_SUBSEQUENCE_LEN]
-
-            if len(subsequence) == 0:
-                break
-
-            converted_subsequence = [word_vocabulary.get(w, word_vocabulary[data.UNK]) for w in subsequence]
-
-            y = predict_function(to_array(converted_subsequence), to_array(subsequence_pauses, dtype=theano.config.floatX))
-
-            f_out.write(subsequence[0])
-
-            last_eos_idx = 0
-            punctuations = []
-            for y_t in y:
-
-                p_i = np.argmax(y_t.flatten())
-                punctuation = reverse_punctuation_vocabulary[p_i]
-
-                punctuations.append(punctuation)
-
-                if punctuation in data.EOS_TOKENS:
-                    last_eos_idx = len(punctuations) # we intentionally want the index of next element
-
-            if subsequence[-1] == data.END:
-                step = len(subsequence) - 1
-            elif last_eos_idx != 0:
-                step = last_eos_idx
-            else:
-                step = len(subsequence) - 1
-
-            for j in range(step):
-                f_out.write(" " + punctuations[j] + " " if punctuations[j] != data.SPACE else " ")
-                if j < step - 1:
-                    f_out.write(subsequence[1+j])
-
-            if subsequence[-1] == data.END:
-                break
-
-            i += step
-
-def restore(output_file, text, word_vocabulary, reverse_punctuation_vocabulary, predict_function):
-    i = 0
-    with codecs.open(output_file, 'w', 'utf-8') as f_out:
-        while True:
-
-            subsequence = text[i:i+MAX_SUBSEQUENCE_LEN]
-
-            if len(subsequence) == 0:
-                break
-
-            converted_subsequence = [word_vocabulary.get(w, word_vocabulary[data.UNK]) for w in subsequence]
-
-            y = predict_function(to_array(converted_subsequence))
-
-            f_out.write(subsequence[0])
-
-            last_eos_idx = 0
-            punctuations = []
-            for y_t in y:
-
-                p_i = np.argmax(y_t.flatten())
-                punctuation = reverse_punctuation_vocabulary[p_i]
-
-                punctuations.append(punctuation)
-
-                if punctuation in data.EOS_TOKENS:
-                    last_eos_idx = len(punctuations) # we intentionally want the index of next element
-
-            if subsequence[-1] == data.END:
-                step = len(subsequence) - 1
-            elif last_eos_idx != 0:
-                step = last_eos_idx
-            else:
-                step = len(subsequence) - 1
-
-            for j in range(step):
-                f_out.write(" " + punctuations[j] + " " if punctuations[j] != data.SPACE else " ")
-                if j < step - 1:
-                    f_out.write(subsequence[1+j])
-
-            if subsequence[-1] == data.END:
-                break
-
-            i += step
-
-if __name__ == "__main__":
-
-    if len(sys.argv) > 1:
-        model_file = sys.argv[1]
-    else:
-        sys.exit("Model file path argument missing")
-
-    if len(sys.argv) > 2:
-        output_file = sys.argv[2]
-    else:
-        sys.exit("Output file path argument missing")
-
-    use_pauses = len(sys.argv) > 3 and bool(int(sys.argv[3]))
-
-    x = T.imatrix('x')
-    
-    if use_pauses:
-    
-        p = T.matrix('p')
-
-        print "Loading model parameters..."
-        net, _ = models.load(model_file, 1, x, p)
-
-        print "Building model..."
-        predict = theano.function(
-            inputs=[x, p],
-            outputs=net.y
-        )
-
-    else:
-
+        x = T.imatrix('x')
+        
         print "Loading model parameters..."
         net, _ = models.load(model_file, 1, x)
 
         print "Building model..."
-        predict = theano.function(
+        self.predict = theano.function(
             inputs=[x],
             outputs=net.y
-        )
+            )
 
-    word_vocabulary = net.x_vocabulary
-    punctuation_vocabulary = net.y_vocabulary
+        self.word_vocabulary = net.x_vocabulary
+        self.punctuation_vocabulary = net.y_vocabulary
+        
+        # remap annotated punctuation tokens other than space to just the punctuation mark
+        for key in self.punctuation_vocabulary.keys():
+            if not "SPACE" in key:
+                punc_mark = "".join([char for char in key if not char.isalpha()])
+                self.punctuation_vocabulary[punc_mark] = self.punctuation_vocabulary.pop(key)
 
-    reverse_word_vocabulary = {v:k for k,v in word_vocabulary.items()}
-    reverse_punctuation_vocabulary = {v:k for k,v in punctuation_vocabulary.items()}
+        self.reverse_word_vocabulary = {v:k for k,v in self.word_vocabulary.items()}
+        self.reverse_punctuation_vocabulary = {v:k for k,v in self.punctuation_vocabulary.items()}
 
-    input_text = codecs.getreader('utf-8')(sys.stdin).read()
+    def capitalizer(self,match):
+        """Return a capitalized form of a regex match"""
+        capped = match.group(1).upper()
+        
+        return capped
 
-    if len(input_text) == 0:
-        sys.exit("Input text from stdin missing.")
+    def clean(self,text):
+        """Remove spacing before apostrophes and add basic capitalizations"""
 
-    text = [w for w in input_text.split() if w not in punctuation_vocabulary and w not in data.PUNCTUATION_MAPPING and not w.startswith(data.PAUSE_PREFIX)] + [data.END]
-    pauses = [float(s.replace(data.PAUSE_PREFIX,"").replace(">","")) for s in input_text.split() if s.startswith(data.PAUSE_PREFIX)]
+        subbed = re.sub("(\,|\.+|\?|\!)? '","'",text)
+        # capitalize "I"
+        subbed = re.sub(" i(\W)"," I\\1",subbed)
+        # capitalize first letter of a sentence
+        subbed = re.sub("(?<!\.)([\.\?\!] \w)",self.capitalizer,subbed).strip()
+        # capitalize first letter of file
+        subbed = re.sub("^(\w)",self.capitalizer,subbed)
+        # make sure file ends with EOS token
+        if subbed[-1] not in [".","?","!"]:
+            subbed = ".".join([subbed,""])
+        
+        return subbed
 
-    if not use_pauses:
-        restore(output_file, text, word_vocabulary, reverse_punctuation_vocabulary, predict)
-    else:
-        if not pauses:
-            pauses = [0.0 for _ in range(len(text)-1)]
-        restore_with_pauses(output_file, text, pauses, word_vocabulary, reverse_punctuation_vocabulary, predict)
+    def to_array(self,arr,dtype=np.int32):
+
+        # minibatch of 1 sequence as column
+        return np.array([arr], dtype=dtype).T
+
+    def restore(self,text):
+        i = 0
+        output_segments = []
+
+        while True:
+
+            subsequence = text[i:i+self.MAX_SUBSEQUENCE_LEN]
+
+            if len(subsequence) == 0:
+                break
+
+            converted_subsequence = [self.word_vocabulary.get(w,self.word_vocabulary[data.UNK]) for w in subsequence]
+
+            y = self.predict(self.to_array(converted_subsequence))
+
+            output_segments.append(subsequence[0])
+
+            last_eos_idx = 0
+            punctuations = []
+            for y_t in y:
+
+                p_i = np.argmax(y_t.flatten())
+                punctuation = self.reverse_punctuation_vocabulary[p_i]
+                punctuations.append(punctuation)
+
+                if punctuation in data.EOS_TOKENS:
+                    last_eos_idx = len(punctuations) # we intentionally want the index of next element
+
+            if subsequence[-1] == data.END:
+                step = len(subsequence) - 1
+            elif last_eos_idx != 0:
+                step = last_eos_idx
+            else:
+                step = len(subsequence) - 1
+
+            for j in range(step):
+                output_segments.append(punctuations[j] + " " if punctuations[j] != data.SPACE else " ")
+                if j < step - 1:
+                    output_segments.append(subsequence[1+j])
+
+            if subsequence[-1] == data.END:
+                break
+
+            i += step
+
+        return "".join(output_segments)
+
+    def punctuate(self,input_text):
+
+        text = [w for w in input_text.split() if w not in self.punctuation_vocabulary and w not in data.PUNCTUATION_MAPPING and not w.startswith(data.PAUSE_PREFIX)] + [data.END]
+        pauses = [float(s.replace(data.PAUSE_PREFIX,"").replace(">","")) for s in input_text.split() if s.startswith(data.PAUSE_PREFIX)]
+        
+        result = self.restore(text)
+        cleaned = self.clean(result)
+
+        return cleaned
